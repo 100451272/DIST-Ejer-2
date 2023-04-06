df213a1867240d3d76fdf14e3bdc5bae26885944
ramonvaldes <78918029+ramonvaldes@users.noreply.github.com>
2023-04-06 12:41:54 +0200

    No sé si esto va bien

--- servidor.c
+++ servidor.c
@@ -1,177 +1,80 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <mqueue.h>
 #include <string.h>
-#include <stdbool.h>
-#include <pthread.h>
-#include "list.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-// declare a global mutex
-pthread_mutex_t mutex_mensaje, mutex_lista;
-pthread_cond_t c_cop;
-bool copiado;
-List list;
-mqd_t q_servidor;
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-void petition_handler(void *peticion){
-    mqd_t q_cliente;
-    struct peticion res;
-    // Protegemos la copia de la peticion con mutex
-    pthread_mutex_lock(&mutex_mensaje);
-    struct peticion pet = (*(struct peticion *)peticion);
-    copiado = true;
-    pthread_cond_signal(&c_cop);
-    pthread_mutex_unlock(&mutex_mensaje);
+int server_fd, new_socket, valread;
+struct sockaddr_in address;
+int opt = 1;
+int addrlen = sizeof(address);
+char buffer[BUFFER_SIZE] = {0};
 
-    // Protegemos las operaciones sobre la lista para evitar
-    // condiciones de carrera en la lista
-    pthread_mutex_lock(&mutex_lista);
-    switch (pet.op) {
-        case 0: //INIT
-            init(&list);
-            res.op= 0;
-            break;
-
-        case 1: //SET_VALUE
-            set(&list, pet.tupla.clave, &pet.tupla);
-            res.op = 0;
-            printList(list);
-            break;
-
-        case 2: //GET_VALUE
-            res.op = get(&list, pet.tupla.clave, &pet.tupla);
-            if (res.op == 0) { // si la operación fue exitosa
-                // copiamos los valores de la tupla a los campos de la respuesta
-                strcpy(res.tupla.valor1, pet.tupla.valor1);
-                res.tupla.valor2 = pet.tupla.valor2;
-                res.tupla.valor3 = pet.tupla.valor3;
-            }
-            break;
-
-        case 3: //MODIFY_VALUE
-        if (exists(&list, pet.tupla.clave) == 0) {
-            printf("La clave %d no existe\n", pet.tupla.clave);
-            res.op = -1;
-            break;
-        }
-            delete_node(&list, pet.tupla.clave);
-            set(&list, pet.tupla.clave, &pet.tupla);
-            printList(list);
-            res.op = 0;
-            break;
-
-        case 4: //DELETE_KEY
-            if (delete_node(&list, pet.tupla.clave) == 0) {
-                res.op = 0;
-            } else {
-                res.op = -1;
-            }
-            printList(list);
-            fflush(NULL);
-            break;
-
-        case 5: //EXIST
-            res.op = exists(&list, pet.tupla.clave);
-            if (res.op == 0) {
-                printf("No existe\n");
-            }
-            else if (res.op == 1){
-                printf("Existe\n");
-            }
-            printList(list);
-            break;
-
-        case 6: // COPY_KEY
-        {
-            struct tupla tupla1, tupla2;
-            int key1 = pet.tupla.clave;
-            int key2 = pet.tupla.valor2;
-
-            // Buscar la tupla de key1
-            if (get(&list, key1, &tupla1) == -1) {
-                res.op = -1; // La clave key1 no existe
-                break;
-            }
-
-            // Crear la tupla de key2 con los valores de key1
-            tupla2.clave = key2;
-            strcpy(tupla2.valor1, tupla1.valor1);
-            tupla2.valor2 = tupla1.valor2;
-            tupla2.valor3 = tupla1.valor3;
-
-            // Insertar o modificar la tupla en la lista
-            delete_node(&list, tupla2.clave);
-            set(&list, key2, &tupla2);
-
-            res.op = 0;
-            printList(list);
-            break;
-        }
-    }
-    // Liberamos el mutex de la lista para que puedan acceder otros
-    // threads a la lista
-    pthread_mutex_unlock(&mutex_lista);
-    q_cliente = mq_open("/CLIENTE", O_WRONLY);
-		if (q_cliente < 0) {
-			perror("mq_open");
-			mq_close(q_servidor);
-			mq_unlink("/ALMACEN");
-		}
-        else {
-            if (mq_send(q_cliente, (const char *)&res, sizeof(res), 0) < 0) {
-                perror("mq_send");
-                mq_close(q_servidor);
-                mq_unlink("/ALMACEN");
-                mq_close(q_cliente);
-            }
-            fflush(NULL);
-        }
-    pthread_exit(0);
+// Crear el socket
+if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
 }
 
+// Configurar opciones del socket
+if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+}
 
+// Configurar dirección del servidor
+address.sin_family = AF_INET;
+address.sin_addr.s_addr = INADDR_ANY;
+address.sin_port = htons(PORT);
 
-int main(void){
-    struct peticion pet;
-    struct mq_attr attr;
-    pthread_attr_t t_attr;
-   	pthread_t thid;
+// Asignar dirección al socket
+if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+}
 
-    attr.mq_maxmsg = 10;                
-	attr.mq_msgsize = sizeof(struct peticion);
-    q_servidor = mq_open("/ALMACEN", O_CREAT|O_RDONLY, 0700, &attr);
-	if (q_servidor == -1) {
-		perror("mq_open");
-		return -1;
-	}
-    pthread_mutex_init(&mutex_mensaje, NULL);
-	pthread_cond_init(&c_cop, NULL);
-    pthread_mutex_init(&mutex_lista, NULL);
-	pthread_attr_init(&t_attr);
+// Escuchar conexiones entrantes
+if (listen(server_fd, 3) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+}
 
-	pthread_attr_setdetachstate(&t_attr, PTHREAD_CREATE_DETACHED);
+while (1) {
+    // Aceptar conexión entrante
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+    perror("accept");
+    exit(EXIT_FAILURE);
+    }
 
-        while(1) {
-        if (mq_receive(q_servidor, (char *) &pet, sizeof(pet), 0) < 0){
-			perror("mq_recev");
-			return -1;
-		}
-        printf("Peticion recibida: %d\n", pet.op);
-        if (pthread_create(&thid, &t_attr, (void *)petition_handler, (void *)&pet)== 0) {
-			// se espera a que el thread copie el mensaje 
-			pthread_mutex_lock(&mutex_mensaje);
-			while (!copiado)
-				pthread_cond_wait(&c_cop, &mutex_mensaje);
-			copiado = false;
-			pthread_mutex_unlock(&mutex_mensaje);
-	 	}  
-        fflush(NULL);
-        //printf("%d", res.op);
-                /* se responde al cliente abriendo reviamente su cola */
-        }   
-    mq_close(q_servidor);
-    // Remove the message queue from the system
-    mq_unlink("/ALMACEN");
-    return 0;
+    // Leer datos enviados por el cliente
+    valread = read(new_socket, buffer, BUFFER_SIZE);
+
+    // Procesar solicitud del cliente
+    if (strncmp(buffer, "init", 4) == 0) {
+    // Llamar a la función init
+    } else if (strncmp(buffer, "set", 3) == 0) {
+    // Llamar a la función set_value
+    } else if (strncmp(buffer, "get", 3) == 0) {
+    // Llamar a la función get_value
+    } else if (strncmp(buffer, "modify", 6) == 0) {
+    // Llamar a la función modify_value
+    } else if (strncmp(buffer, "delete", 6) == 0) {
+    // Llamar a la función delete_key
+    } else if (strncmp(buffer, "exist", 5) == 0) {
+    // Llamar a la función exist
+    } else if (strncmp(buffer, "copy", 4) == 0) {
+    // Llamar a la función copy_key
+    }
+
+    // Limpiar buffer
+    memset(buffer, 0, BUFFER_SIZE);
+
+    // Cerrar conexión
+    close(new_socket);
 }
